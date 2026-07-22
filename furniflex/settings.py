b4086ee79cb382684a07e6f5c578c6905200fd47
Nikhil-1107 <rathodnikhil1107@gmail.com>
2026-07-22 12:12:49 +0530

    Configure project for Vercel deployment

--- furniflex/settings.py
+++ furniflex/settings.py
@@ -26,11 +26,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    ".vercel.app",
-]
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="127.0.0.1,localhost,.vercel.app,*,furni-flex-pied.vercel.app",
+    cast=lambda v: [h.strip() for h in v.split(",")]
+)
 
 
 # Application definition
