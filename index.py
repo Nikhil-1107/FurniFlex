3cfeedaf94b245a6baa5da64fe58c3bde085b518
Nikhil-1107 <rathodnikhil1107@gmail.com>
2026-07-27 15:02:56 +0530

    Test minimal WSGI handler on Vercel

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-from furniflex.wsgi import app
+import sys
+import os
+
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain; charset=utf-8')]
+    start_response(status, headers)
+    info = f"Hello World from Vercel Python!\nPython Version: {sys.version}\nCWD: {os.getcwd()}"
+    return [info.encode('utf-8')]
